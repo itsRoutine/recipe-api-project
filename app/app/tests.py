44ac637a9ab609caa5209dff32af104fb8ff5aa9
Moein Rasouli <mmdmoeinras@gmail.com>
2024-05-18 16:09:28 +0330

    installed postgres adaptor and set the default database

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        res = calc.add(3, 8)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res, -5)
