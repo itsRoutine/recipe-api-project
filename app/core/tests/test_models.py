6386f28aa9f9be2b0a0c7f9846b0608b7e014dff
Moein Rasouli <mmdmoeinras@gmail.com>
2024-05-26 17:36:40 +0330

    add superuser support

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,3 +30,9 @@ class ModelTests(TestCase):
     def test_new_user_without_email_raises_error(self):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'sample123')
+
+    def test_create_superuser(self):
+        user = get_user_model().objects.create_superuser('test@example.com', 'sample123')
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
