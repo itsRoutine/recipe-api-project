9af368c1c8cf2d7b908f90cd7ddd2094601a2483
Moein Rasouli <mmdmoeinras@gmail.com>
2024-05-26 17:31:36 +0330

    make email field required

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,3 +26,7 @@ class ModelTests(TestCase):
         for email, excepted in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, excepted)
+
+    def test_new_user_without_email_raises_error(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'sample123')
