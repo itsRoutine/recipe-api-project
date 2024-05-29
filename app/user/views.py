686672eb929dfc82cc173b2716879aab42060600
Moein Rasouli <mmdmoeinras@gmail.com>
2024-05-29 11:52:06 +0330

    add user management api

--- app/user/views.py
+++ app/user/views.py
@@ -3,7 +3,7 @@ from user.serializers import (
     AuthTokenSerializer
 )
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -15,3 +15,12 @@ class CreateUserView(generics.CreateAPIView):
 class CreateTokenView(ObtainAuthToken):
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
